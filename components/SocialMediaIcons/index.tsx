17d78b36480c001d570936fffbbecde9d3beaaf7
ADamian17 <adonismartin90@gmail.com>
2024-06-21 21:39:57 -0700

    added socialmedia icons

---
+++ components/SocialMediaIcons/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Link from "next/link";
+
+import styles from "./SocialMediaIcons.module.scss";
+
+type SocialMediaIconsType = {};
+
+const SocialMediaIcons: React.FC<SocialMediaIconsType> = (props) => (
+  <div className={styles.socialMediaIcons}>
+    <Link
+      className={styles.socialMediaLink}
+      href='/https://www.linkedin.com/in/margot-martin-realtor'
+      rel="nofollow"
+      target='_blank'
+    >
+      <svg className={styles.socialMediaIcon}>
+        <use href='/icons/social-media-defs.svg#linkedin' />
+      </svg>
+    </Link>
+
+    <Link
+      className={styles.socialMediaLink}
+      href='https://www.instagram.com/margotmartinthebay'
+      rel="nofollow"
+      target='_blank'
+    >
+      <svg className={styles.socialMediaIcon}>
+        <use href='/icons/social-media-defs.svg#instagram' />
+      </svg>
+    </Link>
+
+    <Link
+      rel="nofollow"
+      className={styles.socialMediaLink}
+      href='https://www.grubbco.com/agent-profile/margot-martin-109779059'
+      target='_blank'
+    >
+      <svg className={styles.socialMediaIcon}>
+        <use href='/icons/social-media-defs.svg#external-link' />
+      </svg>
+    </Link>
+  </div>
+)
+
+export default SocialMediaIcons;
