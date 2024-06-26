a8ce40535c9d3fed49968fbcc49b067295e60cb8
ADamian17 <adonismartin90@gmail.com>
2024-06-25 20:55:43 -0700

    added FooterSocialMediaSection

---
+++ components/FooterSocialMediaSection/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+
+import styles from "./FooterSocialMediaSection.module.scss";
+import Image from "next/image";
+import DreAndEmail from "../DreAndEmail";
+import SocialMediaIcons from "../SocialMediaIcons";
+
+type FooterSocialMediaSectionProps = {
+  data: {
+    footerLogo: Record<"altText" | "mediaItemUrl", string>;
+  };
+};
+
+const FooterSocialMediaSection: React.FC<FooterSocialMediaSectionProps> = ({ data }) => {
+  return (
+    <section className={styles.socialMediaSection}>
+      <Link href='/' className={styles.logo}>
+        <svg className={styles.logoImg}>
+          <use href="/icons/main-logos-defs.svg#maim-logo"></use>
+        </svg>
+      </Link>
+
+      <DreAndEmail />
+
+      <SocialMediaIcons />
+    </section>
+  )
+}
+
+export default FooterSocialMediaSection;
