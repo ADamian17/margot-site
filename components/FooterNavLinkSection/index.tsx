9afd6ca28ef61b5c537f30d484c7eb4d3087eca6
ADamian17 <adonismartin90@gmail.com>
2024-07-19 19:23:13 -0700

    updated footer

--- components/FooterNavLinkSection/index.tsx
+++ components/FooterNavLinkSection/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 import styles from "./FooterNavLinkSection.module.scss";
+import { Title } from "@mantine/core";
 
 type FooterNavLinkSectionProps = {
   data: {
@@ -13,13 +14,13 @@ type FooterNavLinkSectionProps = {
 const FooterNavLinkSection: React.FC<FooterNavLinkSectionProps> = ({ data }) => {
   const navLinks = data.navLinks.map(item => (
     <li key={item?.navLink?.urlText}>
-      <Link href={item?.navLink?.urlPath ?? ''}>{item?.navLink?.urlText}</Link>
+      <Link className={styles.itemLink} href={item?.navLink?.urlPath ?? ''}>{item?.navLink?.urlText}</Link>
     </li>
   ));
 
   return (
     <section className={styles.quickLinksSection}>
-      <h3 className={styles.quickLinksTitle}>{data?.headline}</h3>
+      <Title order={2} size={40} style={{ textTransform: "uppercase", letterSpacing: 8 }}>{data?.headline}</Title>
 
       <ul className={styles.linksSection}>
         {navLinks}
