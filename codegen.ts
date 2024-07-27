93614ac5351262cb7354ed79d9e1c120d5956e9d
ADamian17 <adonismartin90@gmail.com>
2024-07-27 10:34:11 -0700

    updated codegen config

--- codegen.ts
+++ codegen.ts
@@ -1,5 +1,5 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
-import {loadEnvConfig} from "@next/env"
+import { loadEnvConfig } from "@next/env";
 
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
